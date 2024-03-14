fd324cb511ec33398b67fc9a31f0a0ebfc4721de
srikanth <srikanthrajcm@gmail.com>
2024-03-14 16:02:28 -0500

    this is commit from GitDemo

--- src/first_Testng.java
+++ src/first_Testng.java
@@ -5,6 +5,8 @@ public class first_Testng {
 	@Test
 	public void demo() {
 		System.out.println("Hello");
+		System.out.println("Hello1");
+		System.out.println("Hello2");
 	}
 	
 	//public static void main(String[] args) {
